b66b7eb3094b0d997b72c014a7bd2e14b97c7835
Rubenscode <rubens.andrade1@gmail.com>
2020-10-06 22:41:49 -0300

    Add enunciados dos exercicios

--- Lista1/mediaFinalAluno.java
+++ Lista1/mediaFinalAluno.java
@@ -1,3 +1,6 @@
+/* 5. Faça um sistema que leia as 3 notas de um aluno e calcule a média final deste
+aluno. Considerar que a média é ponderada e que o peso das notas é: 2,3 e 5,
+respectivamente. */
 package exercicios;
 
 import java.util.Scanner;
